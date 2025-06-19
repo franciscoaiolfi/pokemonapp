a2628a7f856fa6eb6ac10913aedb4eb79c6f0863
franciscoaiolfi <chicoaiolfi12@gmail.com>
2025-06-19 13:46:59 -0300

    feat: new pokemon service - done

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { PokemonService } from 'src/app/services/pokemon/pokemon.service';
 
 @Component({
   selector: 'app-home',
@@ -11,10 +12,26 @@ import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/stan
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class HomePage implements OnInit {
+  pokemons: any[]=[];
+  limit = 20;
+  offset = 0;
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService) { }
 
   ngOnInit() {
+    this.loadPokemons()
+  }
+
+  loadPokemons(){
+    this.pokemonService.getPokemonsList(this.offset,this.limit).subscribe({
+      next:(response)=>{
+        this.pokemons = response
+        console.log(this.pokemons)
+      },
+      error:(err)=>{
+        console.log('deu erro ')
+      }
+    })
   }
 
 }
--- src/app/services/pokemon/pokemon.service.ts
+++ src/app/services/pokemon/pokemon.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PokemonService {
+  private baseUrl = environment.apiUrl
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  getPokemonsList(offset:number = 0, limit:number = 20):Observable<any>{
+    return this.http.get<any>(`${this.baseUrl}?offset=${offset}&limit=${limit}`)
+  }
+
+  getPokemonByName(name:string){
+    return this.http.get<any>(`${this.baseUrl}/${name}`)
+  }
 }
